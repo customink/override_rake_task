30cb501a03f65e835c44c0716eca2ecbf9ad1979
Derek Harmel <dgharmel@gmail.com>
2010-08-16 12:06:06 -0400

    Rails 3 fix: Use task name from Rake.application.lookup instead of .resolve_args because the latter doesn't include the whole scope

--- lib/override_rake_task.rb
+++ lib/override_rake_task.rb
@@ -10,7 +10,8 @@ def alias_task(old_name, new_name)
 end
 
 def override_task(*args, &block)
-  name, params, deps = Rake.application.resolve_args(args.dup)
+  name = Rake.application.lookup args.first
+  _, params, deps = Rake.application.resolve_args(args.dup)
   alias_task name.to_s, "#{name}:original"
   Rake::Task.define_task(*args, &block)
 end
