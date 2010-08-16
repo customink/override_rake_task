5eeb51ae0a3ec38fc6aafe1297c3b03164b485b1
Derek Harmel <dgharmel@gmail.com>
2010-08-16 12:03:18 -0400

    Rails 3 fix: RAILS_ROOT --> Rails.root

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
-# Insert line: [require "#{RAILS_ROOT}/vendor/plugins/override_rake_task/lib/override_rake_task.rb"]
+# Insert line: [require "#{Rails.root}/vendor/plugins/override_rake_task/lib/override_rake_task.rb"]
 # before "require 'rake'" in Rakefile
 # stolen from app_confing plugin :) http://jarmark.org/projects/app-config/ 
 
@@ -12,7 +12,7 @@ end
 # 'Modifying a File in Place Without a Temporary File'
 output= ""
 inserted = false
-line_to_insert = %q{require "#{RAILS_ROOT}/vendor/plugins/override_rake_task/lib/override_rake_task.rb"}
+line_to_insert = %q{require "#{Rails.root}/vendor/plugins/override_rake_task/lib/override_rake_task.rb"}
 line_to_find = "require 'rake'"
 
 File.open(file, 'r+') do |f|   # open file for update
